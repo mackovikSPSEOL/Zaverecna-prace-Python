0d4285c29f0fa71045ebcf92f48da5300003b9ad
Mates Mackovík <mac42396@spseol.cz>
2024-05-26 01:37:18 +0200

    added secret

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ pygame.mixer.init()
 sirka, vyska = 1600, 900
 screen = pygame.display.set_mode((sirka, vyska)) # nastavení velikosti okna
 pozadi = pygame.image.load("assets/background_krbec.jpg") # nahrání pozadí
+pozadi_secret = pygame.image.load("assets/background_secret.png")
 obrazky = [] 
 obrazky_2 = []
 nasobek = 1
 jeho_projekty = 0
 count = 0
-font = pygame.font.Font(None, 36)
-text = font.render(f'Počet projektů: {jeho_projekty}', True, (255, 255, 255))
+switch = 0
+secret_audio = pygame.mixer.Sound("assets/audio/hello_there.mp3")
+
 
     
 
@@ -35,8 +37,9 @@ def list_obrazku(slozka, pocet_obrazku):
             obrazky_2.append(obrazky_mensi)
     return obrazky_2 #vrací seznam s adresami obrázků pro další použítí
 
+list_obrazku("animace_hlavniho_obrazku", 15)
 def animace(pocet_obrazku, poloha_x, poloha_y):
-    list_obrazku("animace_hlavniho_obrazku", 15)
+    
     for cislo in range(pocet_obrazku):
         snimek = obrazky_2[cislo]
         screen.blit(pozadi,(0,0))
@@ -54,8 +57,9 @@ def animace(pocet_obrazku, poloha_x, poloha_y):
 screen.blit(pozadi, (0, 0))
 
 # pygame.draw.rect(screen, (255, 255, 100), (145, 210, 170, 500), 0) #hlava pana krbce ohraniceni - pro klikani
-
-
+after_click_audio = pygame.mixer.Sound("assets/audio/hejhou.mp3")
+pozadi_hudba = pygame.mixer.Sound("assets/audio/pozadi_hudba.mp3")
+pozadi_hudba.play(loops=-1)
 while True:
     
     for event in pygame.event.get():
@@ -63,12 +67,12 @@ while True:
             pygame.quit()
             sys.exit()
     if event.type == pygame.MOUSEBUTTONDOWN:
-        
+        print(event.pos) 
         if event.pos[0] > 145 and event.pos[0] < 300 and event.pos[1] > 210 and event.pos[1] < 730:
-                # print("vevnitr", event.pos) tohle bylo použito k určení rozměrů, kde se nachází postava pana krbce
+                
           
             if count == 0:
-                after_click_audio = pygame.mixer.Sound("assets/audio/hejhou.mp3").play()
+                after_click_audio.play()
                 time.sleep(0.001)
                 jeho_projekty += 1 * nasobek
                 count -= 1
@@ -77,5 +81,18 @@ while True:
             animace(14, 700, 700)
             count += 1
             screen.blit(pozadi, (0, 0))
-
+        if event.pos[0] > 5 and event.pos[0] < 100 and event.pos[1] > 783 and event.pos[1] < 890:
+            print("trefa")
+            screen.blit(pozadi_secret, (0, 0))
+            
+            if switch == 0:
+                time.sleep(0.2)
+                pozadi_hudba.stop()
+                secret_audio.play()
+                switch += 1
+            else:
+                time.sleep(0.2)
+                screen.blit(pozadi, (0, 0))
+                switch -= 1
+                pozadi_hudba.play(loops=-1)
     pygame.display.flip()
