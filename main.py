4c9661a30379e3d2e803c48820e3f9f4386dcd24
Mates Mackovík <mac42396@spseol.cz>
2024-05-26 02:01:47 +0200

    totalne useless secret, ktery asi smazu

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ pygame.mixer.init()
 sirka, vyska = 1600, 900
 screen = pygame.display.set_mode((sirka, vyska)) # nastavení velikosti okna
 pozadi = pygame.image.load("assets/background_krbec.jpg") # nahrání pozadí
-pozadi_secret = pygame.image.load("assets/background_secret.png")
+pozadi_secret_1 = pygame.image.load("assets/k_animaci\kamna/cislo0.png")
+pozadi_secret_2 = pygame.image.load("assets/k_animaci\kamna/cislo1.png")
 obrazky = [] 
 obrazky_2 = []
 nasobek = 1
@@ -18,6 +19,7 @@ jeho_projekty = 0
 count = 0
 switch = 0
 secret_audio = pygame.mixer.Sound("assets/audio/hello_there.mp3")
+secret_audio2 = pygame.mixer.Sound("assets/audio/final_final.mp3")
 
 
     
@@ -55,7 +57,9 @@ def animace(pocet_obrazku, poloha_x, poloha_y):
             sys.exit()
 
 screen.blit(pozadi, (0, 0))
-
+pozadi_secret_1 = pygame.transform.scale(pozadi_secret_1, (sirka, vyska))
+pozadi_secret_2 = pygame.transform.scale(pozadi_secret_2, (sirka, vyska))
+pygame.display.flip()
 # pygame.draw.rect(screen, (255, 255, 100), (145, 210, 170, 500), 0) #hlava pana krbce ohraniceni - pro klikani
 after_click_audio = pygame.mixer.Sound("assets/audio/hejhou.mp3")
 pozadi_hudba = pygame.mixer.Sound("assets/audio/pozadi_hudba.mp3")
@@ -83,14 +87,26 @@ while True:
             screen.blit(pozadi, (0, 0))
         if event.pos[0] > 5 and event.pos[0] < 100 and event.pos[1] > 783 and event.pos[1] < 890:
             print("trefa")
-            screen.blit(pozadi_secret, (0, 0))
+            
             
             if switch == 0:
-                time.sleep(0.2)
+                screen.blit(pozadi_secret_1, (0, 0))
                 pozadi_hudba.stop()
+                time.sleep(0.5)
+                secret_audio2.play(loops=-1)
                 secret_audio.play()
+                for i in range(0, 20):
+                    screen.blit(pozadi_secret_1, (0, 0))
+                    pygame.display.flip()
+                    time.sleep(0.5)
+                    screen.blit(pozadi_secret_2, (0, 0))
+                    pygame.display.flip()
+                    time.sleep(0.5)
+                
                 switch += 1
+                
             else:
+                secret_audio2.stop()
                 time.sleep(0.2)
                 screen.blit(pozadi, (0, 0))
                 switch -= 1
