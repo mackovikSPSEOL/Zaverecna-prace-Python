1241dc1a9b0dbda3f4d45f7adc6b9147a1ae9d81
Mates Mackovík <mac42396@spseol.cz>
2024-05-30 19:45:56 +0200

    přídání hudby, předělání pořadí proměnných

--- main.py
+++ main.py
@@ -7,34 +7,34 @@ from pygame import draw
 
 pygame.init()
 pygame.mixer.init()
-# pozadi_hudba = pygame.mixer.Sound("assets\hudba\pozadi_hudba.wav")
-sirka, vyska = 1600, 900
-obraz = pygame.display.set_mode((sirka, vyska)) # nastavení velikosti okna
 pozadi = pygame.image.load("assets/background_krbec.jpg") # nahrání pozadí
 pozadi_secret_1 = pygame.image.load("assets/k_animaci\kamna/cislo0.png")
 pozadi_secret_2 = pygame.image.load("assets/k_animaci\kamna/cislo1.png")
+cesta_k_fontu = "assets/cesta_k_fontu/Ubuntu_Sans_Mono/static/UbuntuSansMono-Bold.ttf"
+secret_audio = pygame.mixer.Sound("assets/audio/hello_there.mp3")
+secret_audio2 = pygame.mixer.Sound("assets/audio/final_final.mp3")
+after_click_audio = pygame.mixer.Sound("assets/audio/hejhou.mp3")
+pozadi_hudba = pygame.mixer.Sound("assets/audio/pozadi_hudba.mp3")
+
+sirka, vyska = 1600, 900
 obrazky = [] 
 obrazky_2 = []
 nasobek = 1
 jeho_projekty = 0
 count = 0
 switch = 0
-cesta_k_fontu = "assets/cesta_k_fontu/Ubuntu_Sans_Mono/static/UbuntuSansMono-Bold.ttf"
-secret_audio = pygame.mixer.Sound("assets/audio/hello_there.mp3")
-secret_audio2 = pygame.mixer.Sound("assets/audio/final_final.mp3")
 font_text = pygame.font.Font(cesta_k_fontu, 36)
 text = font_text.render(f'Jeho projekty: {jeho_projekty}', True, (255, 255, 255))
-
 obchod_text = font_text.render('Obchod', True, (255, 255, 255))
 pozadi_secret_1 = pygame.transform.scale(pozadi_secret_1, (sirka, vyska))
 pozadi_secret_2 = pygame.transform.scale(pozadi_secret_2, (sirka, vyska))
-after_click_audio = pygame.mixer.Sound("assets/audio/hejhou.mp3")
-pozadi_hudba = pygame.mixer.Sound("assets/audio/pozadi_hudba.mp3")
 pozadi_hudba.play(loops=-1)
-
 obchod_button = pygame.Rect(800, 30, 100, 120) 
 krbec_button = pygame.Rect(130, 270, 198, 390)
 
+
+
+obraz = pygame.display.set_mode((sirka, vyska)) # nastavení velikosti okna
 
 def refresh_obrazu():
     obraz.blit(pozadi, (0, 0))
@@ -61,6 +61,12 @@ def list_obrazku(slozka, pocet_obrazku):
     return obrazky_2 #vrací seznam s adresami obrázků pro další použítí (zárověň menší naformátování)
 list_obrazku("animace_hlavniho_obrazku", 15) #načtení obrázků pro animaci
 
+def obchod():
+    global jeho_projekty
+    global nasobek
+
+    
+    refresh_obrazu()
 
 def animace(pocet_obrazku, poloha_x, poloha_y):
     global text
@@ -89,9 +95,9 @@ while True:
         # print(event.pos) kontrola pozic
         if obchod_button.collidepoint(event.pos):  # Pokud bylo kliknuto na tlačítko
             # print("trefa_shop") kontrola pozic
-            pozadi_hudba.play(1)
+            pozadi_hudba.stop()
             time.sleep(0.5)
-            
+
         if krbec_button.collidepoint(event.pos):      
             # print("trefa_krbec") kontrola pozic
             after_click_audio.play()
@@ -119,12 +125,7 @@ while True:
             else:
                 secret_audio2.stop()
                 time.sleep(0.2)
-                refresh_obrazu()
                 pygame.display.flip()
                 switch -= 1
                 pozadi_hudba.play(loops=-1)
-            
-            #pozice obchodu
-    
-    refresh_obrazu()
-    pygame.display.flip()
+        refresh_obrazu()
